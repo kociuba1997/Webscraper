252e57fce4988eb60d72809fb00d8a72920eb75e
Dawid Kudela <dawid.kudela@rsqtechnologies.com>
2019-03-25 20:51:26 +0100

    Init Android Project
    hello world!

---
+++ Android/app/src/main/java/com/example/newsscraper/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.newsscraper.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newsscraper.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
