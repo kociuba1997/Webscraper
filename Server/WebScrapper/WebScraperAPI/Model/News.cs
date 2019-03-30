55b9a1427cdf27f7dd85221e018bd3019e18b478
kubakolas <kubakolas12@gmail.com>
2019-03-30 12:37:33 +0100

    Add REST Api, connect to MongoDB

---
+++ Server/WebScrapper/WebScraperAPI/Model/News.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScraperAPI.Model
+{
+    public class News
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id;
+        public string[] tags = new string[] { };
+        public string author;
+        public string text;
+        public string link;
+    }
+}
